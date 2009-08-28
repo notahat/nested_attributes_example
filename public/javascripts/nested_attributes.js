c6858c8b0aa8854b7a69a9fa501b0636d2fe7728
Pete Yandell <pete@notahat.com>
2009-08-28 13:14:48 +1000

    Got rid of the magic with the rel attribute.
    The addNestedItem and removeNestedItem functions were looking at the
    href and rel attributes to work out what elements they should be acting
    on. Apart from being an abuse of those attributes, it was really pushing
    functionality down into the HTML that shouldn't have been there.
    
    Now the addNestedItem adn removeNestedItem functions are generators.
    They take the elements to be acted on, and return functions that can be
    used as click handlers. Much neater.

--- public/javascripts/nested_attributes.js
+++ public/javascripts/nested_attributes.js
@@ -1,62 +1,59 @@
 /*
- * This is an onClick handler for adding new nested models to a form.
+ * Returns a function that can be used as a click handler for adding
+ * nested models to a form. For example:
+ *
+ *   $("add_task_link").observe("click", addNestedItem("tasks", taskTemplate));
  * 
- * For example, you might have a link like this:
- *
- *   <a href="#comment" rel="comments">Add a comment</a>
- *
- * If you attach addNestedItem as the click handler for this link, it will:
- *
- * 1. Look for a JavaScript variable named "comment" (taken from the href),
- *    and use its contents as the template for the HTML to be added.
- *
- * 2. Replace all the occurrences of NEW_RECORD in the template with a
+ * The returned click handler will:
+ * 
+ * 1. Replace all the occurrences of NEW_RECORD in the template with a
  *    generated ID (see the generate_template helper.)
  *
- * 3. Append the HTML to the element with id "comments" (taken from the rel).
+ * 2. Insert the template in the bottom of the given container.
+ *
+ * 3. Set the focus to the first form element inside the new item.
  */
-function addNestedItem(event) { 
-  var element = event.element();
+function addNestedItem(container, template) { 
+  return function(event) {
+    container = $(container);
+    template = template.replace(/NEW_RECORD/g, new Date().getTime());
 
-  var template = eval(element.href.replace(/.*#/, ''));
-  template = template.replace(/NEW_RECORD/g, new Date().getTime());
+    container.insert({ bottom: template });
+    var newElement = container.childElements().last();
 
-  var container = $(element.rel);
-  container.insert({ bottom: template });
-  var newElement = container.childElements().last();
+    newElement.down("input,select,textarea").focus();
 
-  newElement.down("input,select,textarea").focus();
-
-  event.stop();
+    event.stop();
+  };
 }
 
 /*
- * This is an onClick handler for removing nested models from a form.
+ * Returns a function that can be used as a click handler for removing
+ * nested models from a form. For example:
  *
- * For example, you might have a link like this:
+ *   $("remove_task_link").observe("click", removeNestedItem("task"));
  *
- *   <a href="#comment">Remove a comment</a>
+ * The returned click handler will:
  *
- * If you attach addNestedItem as the click handler for this link, it will:
+ * 1. Look for the hidden _delete flag immediately before the clicked link
+ *    (as described in Rails' fields_for docs) and set it to 1.
  *
- * 1. Look for a hidden input field immediately before the link (this should
- *    be the _delete attribute, as described in Rails' fields_for docs) and
- *    set it to 1.
- *
- * 2. Either hide the enclosing ".comment" element (class name taken from
- *    the href), or remove it if it's a new record.
+ * 2. Find the enclosing element with a CSS class of itemClass, and either
+ *    hide it, or remove it if it's a new record.
  */
-function removeNestedItem(event) {
-  var element = event.element();
-  var target  = element.up(element.href.replace(/.*#/, '.'));
-  var deleteFlag = element.previous("input.delete_flag");
-  if (deleteFlag) {
-    target.hide();
-    deleteFlag.value = 1;
-  }
-  else {
-    target.remove();
-  }
-  event.stop();
+function removeNestedItem(itemClass) {
+  return function(event) {
+    var element = event.element();
+    var target  = element.up("." + itemClass);
+    var deleteFlag = element.previous("input.delete_flag");
+    if (deleteFlag) {
+      target.hide();
+      deleteFlag.value = 1;
+    }
+    else {
+      target.remove();
+    }
+    event.stop();
+  };
 }
 
